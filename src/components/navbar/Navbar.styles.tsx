c88f3b7c6502df01d32f1fcd7f010eac0aa4e0dc
Leopoldo Nicolás Romero Molines <lnromero@ingenia.es>
2021-02-25 17:17:47 +0100

    T005 titulo del navbar

---
+++ src/components/navbar/Navbar.styles.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+
+export const NavbarContainer =  styled.nav`
+    width: 100%;
+    border-bottom: 1px solid black;
+    display: flex;
+    justify-content: space-around;
+    background-color: #B3B6B7;
+    color: #FBFCFC;
+    align-items: center;
+    padding: 0 1.5rem;
+    text-align: center;
+
+`;
+
+export const Title = styled.h1`
+    font-size: 1.5rem;
+    text-transform: uppercase;
+`;
+
+export const P = styled.p`
+    font-size: 1.5rem;
+    margin-right: 2rem;
+`;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavbarContainer, Title, P} from "./Navbar.styles"
+
+const Navbar = () => {
+    return (
+        <NavbarContainer>
+            <Title>trello</Title>
+            <P>Usuario</P>
+        </NavbarContainer>
+    )
+}
+
+export default Navbar
