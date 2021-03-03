81ca591ae004a6b3d26fd867e639ab465404c0b8
Leopoldo Nicolás Romero Molines <lnromero@ingenia.es>
2021-03-03 13:46:19 +0100

    componente de tarea y sus funcionalidades basicas

--- src/components/taskComponent/TaskComponent.tsx
+++ src/components/taskComponent/TaskComponent.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import {Container, TopContainer, Description, BottomContainer, Button, Title, Input} from "./TaskComponent.styles"
 
-interface TaskProps {
-    task: Todo
+export interface TaskProps {
+    task: Todo;
+    removeTask: RemoveTask;
+    toggleCompleted: ToggleCompleted;
 }
-const TaskComponent: React.FC<TaskProps> = ({task}) => {
+const TaskComponent: React.FC<TaskProps> = ({task, removeTask, toggleCompleted}) => {
+   
     return (
         <Container>
             <TopContainer>
                 <Title>{task.titulo}</Title>
-                <Input type="checkbox" checked={task.completed}/>
+                <Input type="checkbox" onChange={()=>toggleCompleted(task) } checked={task.completed}/>
             </TopContainer>
             <Description>
                 {task.descripcion}
             </Description>
             <BottomContainer>
                 <Button>editar</Button>
-                <Button>eliminar</Button>
+                <Button onClick={()=>removeTask(task)}>eliminar</Button>
             </BottomContainer>
         </Container>
     )
